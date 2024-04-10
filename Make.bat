9f6a1820632a030b0bfd3cfc2b2bdf4359734032
Kafter <115289409+inf2021090@users.noreply.github.com>
2024-04-10 20:40:19 +0300

    Update Make.bat

--- Make.bat
+++ Make.bat
@@ -1,48 +1,30 @@
-# Makefile for setting up Python virtual environment and running Streamlit app
-
-# Commands
-PYTHON = python
-PIP = $(VENV)\Scripts\pip
-STREAMLIT = $(VENV)\Scripts\streamlit
-ACTIVATE = $(VENV)\Scripts\activate
-
-# Virtual environment name
-VENV = venv
-
-# Python requirements file
-REQUIREMENTS = requirements.txt
-
-# Streamlit app file
-APP_FILE = app.py
-
 # Default target
 .PHONY: all
 all: activate
 
-# Create virtual environment
+# create virtual environment
 .PHONY: venv
 venv:
-    $(PYTHON) -m venv $(VENV)
+    python -m venv venv
 
-# Command to activate virtual environment
+#  activate virtual environment
 .PHONY: activate
 activate:
     @echo "Activating virtual environment. Use 'deactivate' to exit."
-    @cmd /k "$(ACTIVATE)"
+    @cmd /k "venv\Scripts\activate"
 
 # Install dependencies
 .PHONY: install
 install: venv
-    $(PIP) install -r $(REQUIREMENTS)
+    venv\Scripts\pip install -r requirements.txt
 
-# Run Streamlit app
+# Run streamlit app
 .PHONY: run
 run: activate
-    $(STREAMLIT) run $(APP_FILE)
+    venv\Scripts\streamlit run app.py
 
-# Clean up virtual environment
+# clean up venv
 .PHONY: clean
 clean:
     @echo "Cleaning up virtual environment and other generated files."
-    @IF EXIST $(VENV) rmdir /s /q $(VENV)
-
+    @IF EXIST venv rmdir /s /q venv
